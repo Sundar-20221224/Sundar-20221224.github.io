85a0e5a6cf4c912bf6f8f38a2c79bef575f7749e
Sundar-20221224 <121343848+Sundar-20221224@users.noreply.github.com>
2022-12-24 23:52:29 +0800

    asdf
    asdfsdafasdf

---
+++ 2048.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <graphics.h>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	initgraph(640, 480);
+	_getch();
+	closegraph();
+	return 0;
+}
---
+++ 画个圆圈/画个圆圈/画个圆圈.cpp
@@ -0,0 +1,12 @@
+﻿#include <graphics.h>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	initgraph(500, 500);
+	circle(250,250,100);
+	_getch();
+	closegraph();
+	return 0;
+}
